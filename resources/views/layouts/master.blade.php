f9b4757788eb19c71e8c0757214c97e4723538b7
Rikki <150494073@qq.com>
2016-09-05 22:40:14 +0800

    Revert "Frank0904"
    This reverts commit 464a8616b8b2baefba4499a4449b9e6ad43f09a1.

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,15 +6,15 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
 	<meta name="viewport" content="width=device-width, initial-scale=1.0,maximum-scale=1.0, user-scalable=no"/>
 	<title>正啊</title>
-	<link rel="stylesheet" type="text/css" href="css/normalize.css">
-	<link rel="stylesheet" type="text/css" href="fonts/iconfont.css">
-	<link rel="stylesheet" type="text/css" href="css/animate.min.css">
-	<link rel="stylesheet" type="text/css" href="css/style.css">
-	<script src='js/jquery-1.7.2.min.js'></script>
-	<script src="js/jquery.lazyload.min.js"></script>
-	<script src="js/wow.min.js"></script>
-	<script src='js/jquery.particleground.min.js'></script>
-	<script src='js/common.js'></script>
+	<link rel="stylesheet" type="text/css" href="/css/normalize.css">
+	<link rel="stylesheet" type="text/css" href="/fonts/iconfont.css">
+	<link rel="stylesheet" type="text/css" href="/css/animate.min.css">
+	<link rel="stylesheet" type="text/css" href="/css/style.css">
+	<script src='/js/jquery-1.7.2.min.js'></script>
+	<script src="/js/jquery.lazyload.min.js"></script>
+	<script src="/js/wow.min.js"></script>
+	<script src='/js/jquery.particleground.min.js'></script>
+	<script src='/js/common.js'></script>
 </head>
 <body>
 	<div id="particles"></div>
